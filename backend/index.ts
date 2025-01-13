edbab633d22527d58adec03494f0c1e9d3ab8a55
Yuuta <github@ranokay.com>
2025-01-13 14:21:31 +0200

    --

--- backend/index.ts
+++ backend/index.ts
@@ -6,13 +6,13 @@ import { weaponsRouter } from "./api/routes/weapons";
 const app = new Elysia()
 	.use(
 		cors({
-			origin: process.env.FRONTEND_URL || "http://localhost:3000",
+			origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
 			credentials: true,
 		}),
 	)
 	.use(armorsRouter)
 	.use(weaponsRouter)
-	.listen(process.env.PORT || 5001);
+	.listen(process.env.PORT ?? 5001);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
